08f5a17a3ba128e928001acb754f8400d757fd0c
thanhtai173 <thanhtai1703@gmail.com>
2021-11-27 16:05:16 +0700

    Not finished yet

---
+++ script-closure-function.js
@@ -0,0 +1,11 @@
+"use strict";
+// Closure function
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
