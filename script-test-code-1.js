b7f1430a11e85fefcf59802da36dbf811ff780b8
thanhtai173 <thanhtai1703@gmail.com>
2021-11-29 09:23:02 +0700

    Example for functions

--- script-test-code-1.js
+++ script-test-code-1.js
@@ -2,35 +2,38 @@ const poll = {
   question: "What is your favorite programming language ?",
   options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
   answers: new Array(4).fill(0),
+  // Initialize function in obj
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n Write option number: `
+      )
+    );
+    // Short coding - Short Circuiting
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    //--------------------------------
+    this.displayResult();
+    this.displayResult("string");
+  },
+  displayResult(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
+  },
 };
 
-const newline = "\n";
+document
+  .querySelector(".answer")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+// Call one function in obj for addEventListener, need to use bind function to which obj
 
-const optionchoice = (obj) => {
-  let stringoption = "";
-  for (const item of obj.options) {
-    stringoption = stringoption + item + newline;
-  }
-  return stringoption;
-};
+// Test data: [5,2,3] [1,5,3,9,6,1]
+const testdata1 = { answers: [5, 2, 3] };
+const testdata2 = { answers: [1, 5, 3, 9, 6, 1] };
 
-const displayResults = (type = "array") => {
-  if (type === "array") {
-    console.log(poll.answers);
-  } else if (type === "string") {
-    console.log(`Poll results are ${poll.answers.join(", ")} `);
-  }
-};
-
-const registerNewAnswer = () => {
-  const choice = Number(prompt(poll.question + newline + optionchoice(poll)));
-  const pollanswer = poll.answers;
-  for (let i = 0; i < pollanswer.length; i++) {
-    pollanswer[i] = i === choice ? pollanswer[i] + 1 : pollanswer[i];
-  }
-
-  displayResults();
-  displayResults("string");
-};
-
-document.querySelector(".answer").addEventListener("click", registerNewAnswer);
+poll.displayResult.call(testdata1, "string");
+poll.displayResult.call(testdata2, "string");
